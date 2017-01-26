27b7aa4870931496327cdb698bfdd5c03efeffe2
Tensaiz <matevy1@gmail.com>
2017-01-26 16:30:44 +0100

    Add working scripts to the game for training - run.py bugging for now

--- gym/envs/space_fortress/constants.py
+++ gym/envs/space_fortress/constants.py
@@ -9,6 +9,32 @@ class KeyMap(Enum):
 	RIGHT=65363
 	SHOOT=32
 	
+class ScriptsSF(Enum):
+        # Sample scripts for now
+        # Script 1: Move left 2 times then shoot
+        SCRIPT1=[KeyMap.LEFT.value,KeyMap.LEFT.value,KeyMap.SHOOT.value]
+        # Script 2: Move right 2 times then shoot
+        SCRIPT2=[KeyMap.RIGHT.value,KeyMap.RIGHT.value,KeyMap.SHOOT.value]
+        # Script 3: Move forward 3 times
+        SCRIPT3=[KeyMap.UP.value,KeyMap.UP.value,KeyMap.UP.value]
+        # Script 4: Move left then forward twice
+        SCRIPT4=[KeyMap.LEFT.value,KeyMap.UP.value,KeyMap.UP.value]
+        # Script 5: Move Right then forward twice
+        SCRIPT5=[KeyMap.RIGHT.value,KeyMap.UP.value,KeyMap.UP.value]
+        
+class ScriptsSFC(Enum):
+        # Sample scripts for now
+        # Script 1: Move left 2 times then right
+        SCRIPT1=[KeyMap.LEFT.value,KeyMap.LEFT.value,KeyMap.RIGHT.value]
+        # Script 2: Move right 2 times then left
+        SCRIPT2=[KeyMap.RIGHT.value,KeyMap.value.RIGHT.value,KeyMap.LEFT.value]
+        # Script 3: Move right 3 times
+        SCRIPT3=[KeyMap.RIGHT.value,KeyMap.RIGHT.value,KeyMap.RIGHT.value]
+        # Script 4: Move left then forward twice
+        SCRIPT4=[KeyMap.LEFT.value,KeyMap.UP.value,KeyMap.UP.value]
+        # Script 5: Move Right then forward twice
+        SCRIPT5=[KeyMap.RIGHT.value,KeyMap.UP.value,KeyMap.UP.value]
+	
 class Games(Enum):
 	SFS="SFS"
 	SF="SF"
